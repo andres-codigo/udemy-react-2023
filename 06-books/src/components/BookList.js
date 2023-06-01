2cb0690dc79f9f2c4d415a11885235152fa66af2
negro-vinilo <52874360+negro-vinilo@users.noreply.github.com>
2023-06-01 14:55:04 +1000

    [js, css] handle add, edit and delete forms and required logic

--- 06-books/src/components/BookList.js
+++ 06-books/src/components/BookList.js
@@ -1,6 +1,12 @@
-function BookList() {
+import BookShow from "./BookShow"
+
+function BookList({ books, onDelete, onEdit }) {
+    const renderedBooks = books.map(book => {
+        return <BookShow onDelete={onDelete} onEdit={onEdit} key={book.id} book={book} />
+    })
+
     return (
-      <div>BookList</div>
+      <div className="book-list">{renderedBooks}</div>
     )
   }
 
